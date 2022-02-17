ff709ab7aa67a47e13a26fd8322228bc030fa434
Yahia <yahia.z.alfeky@gmail.com>
2022-02-17 22:37:52 +0200

    filter by city

--- bikeshare.py
+++ bikeshare.py
@@ -15,4 +15,19 @@ def filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US bikeshare data!\nPlease enter a city to analyze its data.')
+    # get user input for city
+    while True:
+        try:
+            city = input('Please select a city from Chicago, New York City, or Washington.\n').lower()
+        except ValueError:
+            print('Not a valid input. Please select a city from Chicago, New York City, or Washington.')
+        else:
+            if (city=='chicago') or (city=='new york city') or (city=='washington'):
+                print('Selected city is: ',city.title())
+                break
+            else:
+                print('Not a valid city.')
+    
+            
+filters()
