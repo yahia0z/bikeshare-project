be9ff5df52e793f6e912f8e3d34d5da37b3af749
Yahia <yahia.z.alfeky@gmail.com>
2022-02-17 22:36:02 +0200

    define the filters function

--- bikeshare.py
+++ bikeshare.py
@@ -5,3 +5,14 @@ import numpy as np
 data = { 'chicago': 'chicago.csv',
          'new york city': 'new_york_city.csv',
          'washington': 'washington.csv' }
+
+def filters():
+    """
+    Asks user to specify a city, month, and day to analyze.
+
+    Returns:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    """
+    print('Hello! Let\'s explore some US bikeshare data!')
