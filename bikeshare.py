3d940d8370fe659cc235847df1c9909c88c2c9cf
Yahia <yahia.z.alfeky@gmail.com>
2022-02-19 17:55:20 +0200

    calculate time taken to get trip stats

--- bikeshare.py
+++ bikeshare.py
@@ -99,12 +99,13 @@ def trip_stats(df):
     """Displays statistics on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
+    start_time = time.time()
     # total travel time
     total_time = str(datetime.timedelta(seconds = int(df['Trip Duration'].sum())))
     # average travel time
     avg_time = str(datetime.timedelta(seconds = int(df['Trip Duration'].mean())))
     print('Total travel time = {}\nAverage travel time = {}'.format(total_time, avg_time))
-    
+    print("\nThis took {} seconds.".format(time.time() - start_time))
     
 
 city, month, day = filters()
